9b1b06e27beedebdfa01daff91b72307e76d1cb7
Andrei Parv <andrei.parv@ateliere.com>
2024-01-08 16:11:36 +0200

    Update versions to match inventory.json

--- versions.tf
+++ versions.tf
@@ -2,12 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.56.0"
+      version = "2.6.3"
     }
+
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.18.1"
+      version = "4.0.1"
     }
+
     random = {
       source  = "hashicorp/random"
       version = "~> 3.4.3"
